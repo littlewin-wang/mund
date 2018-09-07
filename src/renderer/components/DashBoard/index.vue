4c7c3ae96f3360b36b212927da48cae50d491473
littlewin <littlewin.wang@gmail.com>
2018-09-07 23:58:09 +0800

    feat(dashboard): update control panel

--- src/renderer/components/DashBoard/index.vue
+++ src/renderer/components/DashBoard/index.vue
@@ -1,7 +1,29 @@
 <template>
   <div class="container">
     <div class="control">
-
+      <div class="left">
+        <Dropdown @on-click="changeProject" style="font-size: 14px;">
+          <a href="javascript:void(0)">
+            {{ project || 'Projects' }}
+            <Icon type="arrow-down-b"></Icon>
+          </a>
+          <DropdownMenu slot="list" >
+            <DropdownItem name="Projects">Projects</DropdownItem>
+            <DropdownItem name="vue-cli">vue-cli</DropdownItem>
+            <DropdownItem name="create-react-app">create-react-app</DropdownItem>
+            <DropdownItem name="express-demo">express-demo</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        <div class="count">
+          <span>{{ total }}</span> Total
+        </div>
+      </div>
+      <div class="right">
+        <Input v-model="search" size="small" icon="ios-search" placeholder="Enter something..." style="width: 200px" />
+        <div class="action">
+          <Button type="primary" icon="ios-lightbulb" size="small">Active Project</Button><Button type="success" icon="android-add" size="small" style="margin-left: 10px">New Project</Button>
+        </div>
+      </div>
     </div>
     <div class="main"></div>
   </div>
@@ -9,10 +31,46 @@
 
 <script>
 export default {
-  name: 'dashboard'
+  name: 'dashboard',
+  data () {
+    return {
+      project: '',
+      total: 3,
+      search: ''
+    }
+  },
+  methods: {
+    changeProject (name) {
+      this.project = name
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+  .container {
+    .control {
+      height: 40px;
+      display: flex;
+      .left {
+        height: 100%;
+        flex: 0 0 50%;
+        display: flex;
+        align-items: center;
+        .count {
+          margin-left: 10px;
+        }
+      }
+      .right {
+        height: 100%;
+        flex: 0 0 50%;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        .action {
+          margin-left: 20px;
+        }
+      }
+    }
+  }
 </style>
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,18 +5,14 @@ import App from './App'
 import router from './router'
 import store from './store'
 
-import { Icon, Tabs, TabPane, Breadcrumb, BreadcrumbItem } from 'iview'
+import iView from 'iview'
 import '../../my-theme/dist/iview.css'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
-Vue.component('Icon', Icon)
-Vue.component('Tabs', Tabs)
-Vue.component('TabPane', TabPane)
-Vue.component('Breadcrumb', Breadcrumb)
-Vue.component('BreadcrumbItem', BreadcrumbItem)
+Vue.use(iView)
 
 /* eslint-disable no-new */
 new Vue({
