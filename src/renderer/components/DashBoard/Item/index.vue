a2d058d19fdcdfe35f23d8ebb15f7f32d780008e
littlewin <littlewin.wang@gmail.com>
2018-09-08 16:55:22 +0800

    feat(dashboard): update project item component

--- src/renderer/components/DashBoard/Item/index.vue
+++ src/renderer/components/DashBoard/Item/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="item">
-    <Row type="flex" align="middle" style="height: 100%; font-size: 14px;">
+    <Row class="basic" type="flex" align="middle">
       <Col span="2">
         <div class="type">
           V
@@ -32,6 +32,73 @@
         <Button size="small" type="ghost" shape="circle" icon="chevron-down"></Button>
       </Col>
     </Row>
+    <div class="detail">
+      <div class="line"></div>
+      <Row type="flex" justify="center">
+        <Col span="20" class="project-content">
+          <div class="left">
+            <div class="desc">
+              <span>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </span>
+              <Icon type="edit" style="margin-left: 4px;"></Icon>
+            </div>
+            <div class="version">
+              <Icon class="icon" color="#2d8cf0" size="18" type="ios-information"></Icon>
+              <span>{{ '1.0.0' }}</span>
+            </div>
+            <div class="git">
+              <Icon class="icon" size="18" type="social-github"></Icon>
+              <a>coding-tool/dort</a>
+              <Icon class="icon" size="18" type="network"></Icon>
+              <span>develop</span>
+            </div>
+            <div class="license">
+              <Icon class="icon" color="#ff9900" size="18" type="locked"></Icon>
+              <span>MIT</span>
+            </div>
+          </div>
+          <div class="line">
+          </div>
+          <div class="right">
+            <div class="tag">
+              <h5>Tags</h5>
+              <Tag color="blue">标签一</Tag>
+              <Tag color="green">标签二</Tag>
+              <Tag color="red">标签三</Tag>
+              <Tag color="yellow">标签四</Tag>
+              <Button icon="ios-plus-empty" type="dashed" size="small">
+                Add
+              </Button>
+            </div>
+            <div class="script">
+              <h5>NPM Script</h5>
+              <div class="list">
+                <div class="script-item">
+                  <h5>vue:dev</h5>
+                  <p class="script-cmd">incididunt ut labore et dolore</p>
+                </div>
+                <div class="script-item">
+                  <h5>vue:dev</h5>
+                  <p class="script-cmd">incididunt ut labore et dolore</p>
+                </div>
+                <div class="script-item">
+                  <h5>vue:dev</h5>
+                  <p class="script-cmd">incididunt ut labore et dolore</p>
+                </div>
+                <div class="script-item">
+                  <h5>vue:dev</h5>
+                  <p class="script-cmd">incididunt ut labore et dolore</p>
+                </div>
+                <Button class="script-item" icon="ios-plus-empty" type="dashed">
+                  Add
+                </Button>
+              </div>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </div>
   </div>
 </template>
 
@@ -43,18 +110,92 @@ export default {
 
 <style lang="scss" scoped>
   .item {
-    height: 50px;
     padding: 0 10px;
     background: #34344D;
     border-radius: 6px;
-    .type {
-      width: 30px;
-      height: 30px;
-      line-height: 30px;
-      border-radius: 50%;
-      color: #fff;
-      background: #7ED321;
-      text-align: center;
+    .basic {
+      height: 50px;
+      font-size: 14px;
+      .type {
+        width: 30px;
+        height: 30px;
+        line-height: 30px;
+        border-radius: 50%;
+        color: #fff;
+        background: #7ED321;
+        text-align: center;
+      }
+    }
+    .detail {
+      // height: 200px;
+      .line {
+        height: 1px;
+        background: linear-gradient(to right, transparent, #9b9b9b, transparent)
+      }
+      .project-content {
+        display: flex;
+        justify-content: space-between;
+        .left, .right {
+          flex: 0 0 48%;
+          padding: 10px 0;
+        }
+        .left {
+          .version, .git, .license {
+            margin-top: 4px;
+            height: 24px;
+            display: flex;
+            align-items: center;
+            .icon {
+              margin-right: 10px;
+              &:not(:first-child) {
+                margin-left: 20px;
+              }
+            }
+          }
+        }
+        .right {
+          .tag, .script {
+            h5 {
+              margin-bottom: 4px;
+              font-size: 14px;
+            }
+          }
+          .script {
+            margin-top: 10px;
+            .list {
+              display: flex;
+              flex-wrap: wrap;
+              .script-item {
+                margin: 4px;
+                padding: 0 4px;
+                width: 120px;
+                height: 80px;
+                background: #3b3b56;
+                border-radius: 4px;
+                cursor: pointer;
+                &:hover {
+                  background: #3f3f63;
+                }
+                h5 {
+                  font-weight: normal;
+                  font-size: 14px;
+                }
+                .script-cmd {
+                  padding: 4px 0;
+                  border-top: 1px solid #9b9b9b;
+                  font-size: 12px;
+                  color: #9b9b9b;
+                }
+              }
+            }
+          }
+        }
+        .line {
+          flex: 0 0 1px;
+          height: 100%;
+          background: linear-gradient(to bottom, transparent, #9b9b9b, transparent)
+        }
+      }
     }
   }
 </style>
